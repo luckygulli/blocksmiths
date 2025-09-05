9aa3967025438c8ee84b2571e189f3570dfd6cfc
Guillaume Ménard <guillaume.menard@cofinpro.de>
2025-09-05 15:41:17 +0200

    5 Random players

--- scripts/play.ts
+++ scripts/play.ts
@@ -9,9 +9,16 @@ const Key = TerminalGameIo.Key;
 // Replace with your deployed contract address
 const boardAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const RPC_URL = "http://127.0.0.1:8545";
-const PRIVATE_KEY =
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-  // "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a";
+
+// --- pick a random account from 0..4 ---
+const accounts = [
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+  "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+  "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+  "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a"
+];
+const myPrivateKey = accounts[Math.floor(Math.random() * accounts.length)];
 
 const BOARD_WIDTH = 20;
 const BOARD_HEIGHT = 10;
@@ -25,7 +32,7 @@ const artifact = JSON.parse(
 
 // Setup provider & signer
 const provider = new ethers.JsonRpcProvider(RPC_URL);
-const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+const signer = new ethers.Wallet(myPrivateKey, provider);
 
 // Attach to contract
 const boardContract = new ethers.Contract(boardAddress, artifact.abi, signer);
@@ -134,6 +141,10 @@ async function main() {
     await refreshPositions();
     frameHandler(game);
   });
+
+  setInterval(async () => {
+  await refreshPositions();
+}, 100); // refresh every 100ms
 }
 
 main().catch(console.error);
